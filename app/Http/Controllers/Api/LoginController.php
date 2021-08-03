aa5e5a30434eb430952d2a68bc138f219fcc0f13
Anas M K <anasmk@prompttechsolutions.com>
2021-08-03 22:12:55 +0530

    logout api

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -217,7 +217,7 @@ class LoginController extends Controller
 
     public function logout(){
 
-        // try{
+        try{
             if (Auth::check()) {
                 Auth::user()->AauthAcessToken()->delete();
             }
@@ -229,12 +229,13 @@ class LoginController extends Controller
 
             ], 200);
 
-        // }
-        // catch(\Exception $e){
-        //     return response()->json([
-        //         'status'    => 'error',
-        //         'message'   => 'Something went wrong',
-        //     ], 301);
-        // }
+        }
+        catch(\Exception $e){
+            return response()->json([
+                'status'    => 'error',
+                'message'   => 'Something went wrong',
+            ], 301);
+        }
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('test', [\App\Http\Controllers\Api\LoginController::class, 'newTest']);
 
 Route::get('/', function(){
     return redirect()->route('login.index');
