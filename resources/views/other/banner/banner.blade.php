bdd3415fa1f3a16b2fa5e405a41e40892f871829
“unaisprompt” <unais.vm@prompttechsolutions.com>
2023-07-13 17:29:44 +0530

    test

--- resources/views/other/banner/banner.blade.php
+++ resources/views/other/banner/banner.blade.php
@@ -24,6 +24,7 @@
                             <tr>
                                 <th>#</th>
                                 <th>Name</th>
+                                <th>Country</th>
                                 <th>Status</th>
                                 <th>Action</th>
                             </tr>
@@ -33,6 +34,7 @@
                                 <tr>
                                     <th scope="row">{{ $loop->iteration }}</th>
                                     <td>{{ $row->name }}</td>
+                                    <td>{{ $row->Country->name }}</td>
                                     @if($row->status == 1)
                                     <td class="text-success">Active</td>
                                     @else
