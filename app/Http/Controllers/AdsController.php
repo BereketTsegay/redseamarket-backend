29af9abd1d3ed4d252a1a1ab9c11e30da04b0e5c
“unaisprompt” <unais.vm@prompttechsolutions.com>
2023-07-25 14:36:53 +0530

    test

--- app/Http/Controllers/AdsController.php
+++ app/Http/Controllers/AdsController.php
@@ -1179,7 +1179,7 @@ class AdsController extends Controller
         ->where('delete_status', '!=', Status::DELETE)
         ->orderBy('created_at', 'desc')
         ->paginate(10);
-//    return Status::REREQUEST;
+        return adsRequest;
         $reason = RejectReason::where('status', Status::ACTIVE)
         ->where('type', 0)
         ->orderBy('reson')
