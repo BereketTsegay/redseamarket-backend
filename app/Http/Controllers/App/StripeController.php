23d9246ff58850c2b14651c51789b103a9132981
“unaisprompt” <unais.vm@prompttechsolutions.com>
2023-08-03 17:58:32 +0530

    change

--- app/Http/Controllers/App/StripeController.php
+++ app/Http/Controllers/App/StripeController.php
@@ -18,7 +18,7 @@ class StripeController extends Controller
     private $stripe;
     public function __construct()
     {
-        $this->stripe = new StripeClient(env('STRIPE_SECRET_KEY'));
+        $this->stripe = new StripeClient(config('stripe.api_keys.secret_key'));
     }
 
   
