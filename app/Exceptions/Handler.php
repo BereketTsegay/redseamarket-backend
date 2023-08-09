4679bbac9709a657625143ff491bb25a16fba418
“unaisprompt” <unais.vm@prompttechsolutions.com>
2023-08-09 15:17:51 +0530

    auth exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,12 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+        return redirect()->guest(route('login'));
+    }
 }
