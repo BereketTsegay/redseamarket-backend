35bb483256b42ba685f1473aec8d5033ce17d7eb
“unaisprompt” <unais.vm@prompttechsolutions.com>
2023-08-24 18:19:19 +0530

    add sort

--- app/Http/Controllers/App/OtherController.php
+++ app/Http/Controllers/App/OtherController.php
@@ -2036,7 +2036,7 @@ class OtherController extends Controller
     }
 
     public function searchAlert(){
-        $data=SearchHistory::where('user_id',Auth::user()->id)->latest()->take(5);
+        $data=SearchHistory::where('user_id',Auth::user()->id)->orderBy('id','DESC')->take(5);
          foreach($data as $row){
             $data->ads_count=0;
          }
