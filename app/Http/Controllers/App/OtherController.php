6062cd3c1ee6f7f19cf0773722cc39970574baa2
“unaisprompt” <unais.vm@prompttechsolutions.com>
2023-08-25 15:28:59 +0530

    test

--- app/Http/Controllers/App/OtherController.php
+++ app/Http/Controllers/App/OtherController.php
@@ -2038,17 +2038,17 @@ class OtherController extends Controller
     public function searchAlert(Request $request){
         $country=$request->country ?? 1;
 
-        $data=SearchHistory::where('user_id',Auth::user()->id)->orderBy('id','DESC')->get()->take(5)
-        ->map(function($a,$country){
+        $data=SearchHistory::where('user_id',Auth::user()->id)->orderBy('id','DESC')->get()->take(5);
+        // ->map(function($a,$country){
 
-            $countryAds=AdsCountry::where('country_id',$country)->get()->pluck('ads_id');
+        //     $countryAds=AdsCountry::where('country_id',$country)->get()->pluck('ads_id');
 
-            $myAds = Ads::select('ads.*')
-            ->join('ads_countries','ads_countries.ads_id','ads.id')
-            ->where('ads.status', Status::ACTIVE)
-            ->whereIn('ads.id', $countryAds)->get()->count();
-            $a->ads_count=$myAds;
-        });
+        //     $myAds = Ads::select('ads.*')
+        //     ->join('ads_countries','ads_countries.ads_id','ads.id')
+        //     ->where('ads.status', Status::ACTIVE)
+        //     ->whereIn('ads.id', $countryAds)->get()->count();
+        //     $a->ads_count=$myAds;
+        // });
         return response()->json([
             'status'    => 'success',
             'data'      => $data,
